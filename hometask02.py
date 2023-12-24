7767618c7a70d98e083a1919e2f769ff20ff28f8
MariiaZheliezna <iron_mary@icloud.com>
2023-12-24 21:22:10 +0200

    version 1

---
+++ hometask02.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+s = input('Enter string for palyndrome check >>> ')
+s1 = s.lower().replace(' ','')
+
+q = deque()
+for i in range(len(s1)):
+    q.append(s1[i])
+
+answer = 'Yes, this is palyndrome'
+for i in range(len(q)//2):
+    if q.pop() != q.popleft():
+        answer = 'Not a palyndrome'
+
+print(f'Texst "{s}": {answer}')
